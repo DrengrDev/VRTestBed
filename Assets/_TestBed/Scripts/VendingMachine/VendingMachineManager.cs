a81e166ccef4385391fc24e39fc39db9d5f49793
Jess Salas <drengrdev@gmail.com>
2023-05-09 14:36:34 -0500

    Testing image selection on vending machine

--- Assets/_TestBed/Scripts/VendingMachine/VendingMachineManager.cs
+++ Assets/_TestBed/Scripts/VendingMachine/VendingMachineManager.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class CreditsManager : MonoBehaviour
+public class VendingMachineManager : MonoBehaviour
 {
     public float availableCreds;
     public TextMeshProUGUI credsText;
 
+    public GameObject[] images;
+
+    private GameObject selectedImage;
     private void Start()
     {
         credsText.text = "Available Balance: $" + availableCreds.ToString();
+
+        for(int i = 0; i < images.Length; i++)
+        {
+            images[i].gameObject.SetActive(false);
+        }
     }
 
     private void Update()
@@ -26,5 +34,16 @@ public class CreditsManager : MonoBehaviour
     public void DecreaseCredits(float value)
     {
         availableCreds -= value;
+    }
+
+    public void ChangeImage(int index)
+    {
+        if(selectedImage != null)
+        {
+            selectedImage.SetActive(false);
+        }
+
+        selectedImage = images[index];
+        selectedImage.SetActive(true);
     }
 }
